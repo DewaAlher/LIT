9fa2686a70acab537cf951142da49d6d780870c5
DewaAlher <59711776+DewaAlher@users.noreply.github.com>
2023-06-12 22:05:10 +0700

    ad question controller

--- Models/Question.cs
+++ Models/Question.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
 
         public string AskQuestion { get; set; }
-
+         
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public Question()
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
