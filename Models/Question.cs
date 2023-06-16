941c6f08f58b6f6988cb9e221801569e6526449b
DewaAlher <59711776+DewaAlher@users.noreply.github.com>
2023-06-17 05:49:33 +0700

    changin variable

--- Models/Question.cs
+++ Models/Question.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
 
-        public string AskQuestion { get; set; }
-         
+        public string SomeQuestion { get; set; }
+        public string SomeAnswer { get; set; }
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public Question()
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
